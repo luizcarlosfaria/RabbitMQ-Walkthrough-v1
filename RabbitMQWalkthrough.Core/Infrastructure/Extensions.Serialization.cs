3c9b75eccdf58e27150cb3a349f2d8d0896f80e7
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2021-12-05 01:36:17 -0300

    Troca do Json.NET pelo System.Text.JSON

--- RabbitMQWalkthrough.Core/Infrastructure/Extensions.Serialization.cs
+++ RabbitMQWalkthrough.Core/Infrastructure/Extensions.Serialization.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +9,9 @@ namespace RabbitMQWalkthrough.Core.Infrastructure
 {
     public static partial class Extensions
     {
-        public static string Serialize<T>(this T objectToSerialize) => JsonConvert.SerializeObject(objectToSerialize, Formatting.Indented);
+        public static string Serialize<T>(this T objectToSerialize) => System.Text.Json.JsonSerializer.Serialize<T>(objectToSerialize);
         
-        public static T Deserialize<T>(this string jsonText) => JsonConvert.DeserializeObject<T>(jsonText);
+        public static T Deserialize<T>(this string jsonText) => System.Text.Json.JsonSerializer.Deserialize<T>(jsonText);
 
         public static byte[] ToByteArray(this string text) => System.Text.Encoding.UTF8.GetBytes(text);
 
